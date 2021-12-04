92c9f24787169fde869cee6e71fb651edfce071a
nemenezes <nemenezes@hotmail.com>
2021-12-04 21:35:11 +0000

    part 2 of 1st Dec puzzle

--- day1.py
+++ day1.py
@@ -25,20 +25,24 @@ def type_of_change(diff):
     return output
 
 
-def add_difference(df):
+def add_differences(df):
     df["depth_diff"] = df["depth"].diff(periods=1)
-    df["type_of_change"] = df["depth_diff"].apply(type_of_change)
+    df["depth_rolling_sum"] = df["depth"].rolling(3).sum()
+    df["rolling_sum_diff"] = df["depth_rolling_sum"].diff(periods=1)
+    df["type_of_change_prev_day"] = df["depth_diff"].apply(type_of_change)
+    df["type_of_change_rolling_sum"] = df["rolling_sum_diff"].apply(type_of_change)
     return df
 
 
 def print_changes(df):
     df = get_raw_data()
-    df = add_difference(df)
+    df = add_differences(df)
     print("Change since previous measurement:")  # noqa
-    print(df["type_of_change"].value_counts())  # noqa
+    print(df["type_of_change_prev_day"].value_counts())  # noqa
+    print("Change in rolling sum:")  # noqa
+    print(df["type_of_change_rolling_sum"].value_counts())  # noqa
 
 
 if __name__ == "__main__":
     df = get_raw_data()
-    df = add_difference(df)
     print_changes(df)
