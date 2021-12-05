be26e59a7ac3f78a1f6848953869ae6d8327852b
Nina Menezes <nina.menezes@example.com>
2021-12-05 21:53:10 +0000

    update day3

--- day3.py
+++ day3.py
@@ -1,4 +1,4 @@
-"""Day 3"""
+"""Day 3: Binary Diagnostic"""
 
 import numpy as np
 
@@ -25,13 +25,73 @@ def get_input_data_as_array():
     return matrix
 
 
+def calculate_epsilon_gamma(matrix):
+    # Return epsilon and gamma as strings
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    epsilon = []
+    gamma = []
+
+    for i in range(0, num_cols):
+        col_total = sum(matrix[:,i])
+        if col_total >= (num_rows/2):
+            gamma.append("1")
+            epsilon.append("0")
+        else:
+            gamma.append("0")
+            epsilon.append("1")
+
+    epsilon = "".join(epsilon)
+    gamma = "".join(gamma)
+    return epsilon, gamma
 
 
+def calculate_power_output(epsilon_str, gamma_str):
+    epsilon_decimal = int(epsilon_str, 2)
+    gamma_decimal = int(gamma_str, 2)
+    power = epsilon_decimal * gamma_decimal
+    return power
+    
+
+def calculate_o2_co2_ratings(matrix, o2_generator=True):
+    # Returns string of binary representation
+    num_cols = len(matrix[0])
+    current_matrix = matrix
+    output = []
+    for i in range(0, num_cols):
+        column_total = sum(current_matrix[:,i])
+        num_rows = len(current_matrix)
+        print(column_total)
+        if o2_generator:
+            chosen_val = int((column_total >= (num_rows/2)))
+        else:
+            chosen_val = int((column_total <= (num_rows/2)))
+        print(chosen_val)
+        output.append(chosen_val)
+        mask = (current_matrix[:, i] == chosen_val)
+        current_matrix = current_matrix[mask, :]
+    output = [str(x) for x in output]
+    rating = "".join(output)
+    return rating
 
 
-
-
-
+def calculate_life_support_rating(o2_generator, co2_scrubber):
+    life_support = int(o2_generator, 2) * int(co2_scrubber, 2)
+    return life_support
 
 if __name__ == "__main__":
-    print(get_input_data_as_array())
+    print("Part 1")
+    matrix = get_input_data_as_array()
+    epsilon, gamma = calculate_epsilon_gamma(matrix)
+    print(f"epsilon: {epsilon}")
+    print(f"gamma: {gamma}")
+    power_output = calculate_power_output(epsilon, gamma)
+    print(f"power output: {power_output}")
+    print("-----")
+    print("Part 2")
+    o2_rating = calculate_o2_co2_ratings(matrix, True)
+    co2_rating = calculate_o2_co2_ratings(matrix, False)
+    life_support_rating = calculate_life_support_rating(o2_rating, co2_rating)
+    print(f"oxygen generator rating: {o2_rating}")
+    print(f"CO2 scrubber rating: {co2_rating}")
+    print(f"life support rating: {life_support_rating}")
